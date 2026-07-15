468e3e92fe5579ac5bf657b68ce7c0a521066057
Nigel Ankin <nigelankin@mac.home>
2026-07-15 07:59:08 +0100

    Add Welsh / English toggle in chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import Header from '../../components/Header';
+import Header from '@/components/Header';
 import Link from 'next/link';
 
 export default function Chat() {
